40d2f9e2d2bc773015daeb809355a648b7085a82
Pedro Oliveira <pncolvr@gmail.com>
2026-03-11 20:10:19 +0000

    refactor: replace terminal command execution with child process spawn

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { spawn } from 'child_process';
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,12 +11,8 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('drag-and-drop.runCommandOnFile', async (uri: vscode.Uri, selectedUris?: vscode.Uri[]) => {
 		// uri is the file clicked, selectedUris if multiple files are selected
 		const files = selectedUris && selectedUris.length ? selectedUris : [uri];
-		const terminal = vscode.window.createTerminal("drag-and-drop-terminal");
-		terminal.show();
-
-		// Create space-separated list of file paths wrapped in single quotes
-		const filePaths = files.map(file => `'${file.fsPath}'`).join(' ');
-		terminal.sendText(`dragon-drop ${filePaths}`);
+		const args = files.map(file => file.fsPath);
+		spawn('dragon-drop', args, { detached: true, stdio: 'ignore' });
 	});
 
 	context.subscriptions.push(disposable);
